0cf26df5b9b1b915595449cd45c1d75414499acc
Arshpreet Kaur <arshpreetkaur989@gmail.com>
2018-10-26 10:45:48 -0500

    Calculated Age

--- MvcAssignment2/helpers/ExtensionMethods.cs
+++ MvcAssignment2/helpers/ExtensionMethods.cs
@@ -7,7 +7,7 @@ namespace MvcAssignment2.helpers
 {
     public  static class ExtensionMethods
     {
-        public static string CheckInt(this string str)
+        public static string ToDigitsOnly(this string str)
         {
             var newstr = "";
             foreach (var i in str)
@@ -20,6 +20,12 @@ namespace MvcAssignment2.helpers
             return newstr;
         }
 
-        
+        public static int ToAge(this DateTimeOffset dateOfBirth) {
+            var today = DateTime.Today;
+            var a = (today.Year * 100 + today.Month) * 100 + today.Day;
+            var b = (dateOfBirth.Year * 100 + dateOfBirth.Month) * 100 + dateOfBirth.Day;
+
+            return (a - b) / 10000;
+        }
     }
 }
